2becb2a47199a8b9edfc890ce13be288a92df1f2
Chrisnewgear <chrisnewgear2011@gmail.com>
2021-05-11 00:57:17 -0500

    SOLO FALTA CONFIGURAR EL FORM DE CONTACTO

---
+++ script.js
@@ -0,0 +1,20 @@
+    document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.container', {delay: 500});
+ScrollReveal().reveal('.nav-main', {delay: 500});
+ScrollReveal().reveal('.nav-menu', {delay: 500});
+ScrollReveal().reveal('.showcase', {delay: 500});
+ScrollReveal().reveal('.slider', {delay: 500});
+ScrollReveal().reveal('.tit-servicios', {delay: 500});
+ScrollReveal().reveal('.servicios', {delay: 500});
+ScrollReveal().reveal('.tit-cobertura', {delay: 500});
+ScrollReveal().reveal('.container2', {delay: 500});
+ScrollReveal().reveal('.btn', {delay: 500});
+ScrollReveal().reveal('.tit-tarifario', {delay: 500});
+ScrollReveal().reveal('.tarifario', {delay: 500});
+ScrollReveal().reveal('.tit-contacto', {delay: 500});
+ScrollReveal().reveal('.formulario', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
